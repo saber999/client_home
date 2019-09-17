1cd800c27c86325ac57cbce83cce539bfbe1a84c
jone <876573176@qq.com>
2019-09-17 19:24:20 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Head from './components/index/head.vue';
+import Banner from './components/index/banner.vue';
+import Entries from './components/index/entries.vue';
+import Welfare from './components/index/welfare.vue';
+import Nav from './components/index/nav.vue';
+import Adviser from './components/index/adviser.vue';
+import Aboutus from './components/index/aboutus.vue';
+import Company from './components/index/company.vue';
+import Contact from './components/index/contact.vue';
+import {replaceChn} from './plugins/util.js'
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/head.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="headbar row" v-show="!hideHeadbar">
+        <div class="col"><i class="logo"></i></div>
+        <i class="icon-service"></i>
+        <a href="javascript:;" class="online-service" data-track="index_webCustomerSevice_new">在线客服</a>
+    </section>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            hideHeadbar: false
+        }
+    },
+    mounted() {
+        let userAgent = navigator.userAgent.toLowerCase()
+        if( userAgent.indexOf('mucxiaoyusan') > 0 ){
+            this.hideHeadbar = true;        
+        }
+    }
+}
+</script>
+<style lang="scss">
+    @import '@/assets/css/index/head.scss';
+</style>
+
+
